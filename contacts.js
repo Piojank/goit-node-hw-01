626a44600956b7a6b07722b2979eec6c05dc03b8
piojank <p.jankowski931026@gmail.com>
2023-01-07 20:48:08 +0100

    node homework 1

---
+++ contacts.js
@@ -0,0 +1,27 @@
+const fs = require('fs').promises;
+const path = require('path');
+
+const contactsPath = 
+
+function listContacts() {
+
+}
+
+function getContactById(contactId) {
+
+}
+
+function removeContact(contactId) {
+
+}
+
+function addContact(name, email, phone) {
+
+}
+
+module.exports = {
+        listContacts,
+        getContactById,
+        removeContact,
+        addContact
+};
